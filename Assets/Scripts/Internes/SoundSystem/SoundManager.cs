e758e3b0476789e5233250a4abc081db43caba67
maylis98 <maylis.dh12@gmail.com>
2022-05-05 19:00:02 +0200

    SoundSystem changes + edit in the timeline of Box 1

--- Assets/Scripts/Internes/SoundSystem/SoundManager.cs
+++ Assets/Scripts/Internes/SoundSystem/SoundManager.cs
@@ -9,18 +9,78 @@ public class SoundManager : MonoBehaviour
     public MultiSound soundHeartBeat;
     public MultiSound soundBreath;
     public MultiSound soundWater;
-    public void StartSound()
-    {
-        //if player trigger gate[0]{
 
-        //audioBreath.clip = soundType[0].audioClips[0];
+    private void Start()
+    {
+        boxIsTrigger(false);
     }
 
-    
-   private void EndSound()
-   {
-            
-   }
+    public void boxIsTrigger(bool IsTrigger)
+    {
+        if(IsTrigger == true)
+        {
+            eventWaterRising(true);
+        }
+    }
+
+    public void eventWaterRising(bool changeSound)
+    {
+        StartCoroutine(waterRising());
+
+        if(changeSound == true)
+        {
+            StopAllCoroutines();
+            touchedBox();
+        }
+
+    }
+
+    public void touchedBox()
+    {
+        soundHeartBeat.iCurrentClip = 6;
+        soundWater.iCurrentClip = -1;
+
+    }
+
+    public void heartbeatReturnToDefault()
+    {
+        soundHeartBeat.iCurrentClip = -1;
+    }
+
+    public void waterPresence()
+    {
+        soundWater.iCurrentClip = 2;
+
+        StartCoroutine(stopWaterPresence());
+    }
+
+    public void calmWater()
+    {
+        soundWater.iCurrentClip = -1;
+    }
+
+    IEnumerator waterRising()
+    {
+        soundWater.iCurrentClip = 1;
+        soundHeartBeat.iCurrentClip = 5;
+
+        yield return new WaitForSeconds(5);
+
+        soundHeartBeat.iCurrentClip = 0;
+
+        yield return new WaitForSeconds(25);
+
+        soundWater.iCurrentClip = 0;
+    }
+
+    IEnumerator stopWaterPresence()
+    {
+        yield return new WaitForSeconds(130);
+
+        soundWater.iCurrentClip = -1;
+
+    }
+
 
 
 }
