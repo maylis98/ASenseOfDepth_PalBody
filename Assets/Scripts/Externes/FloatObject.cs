ecdced6cea8a2a6126b3dbbc7200fd3f4a758e8c
maylis98 <maylis.dh12@gmail.com>
2022-05-19 23:35:02 +0200

    Change in the arm appearing after memory 1

--- Assets/Scripts/Externes/FloatObject.cs
+++ Assets/Scripts/Externes/FloatObject.cs
@@ -4,36 +4,101 @@
 
 using UnityEngine;
 using System.Collections;
+using UnityEngine.Events;
 
 // Makes objects float up & down while gently spinning.
 public class FloatObject : MonoBehaviour
 {
     // User Inputs
     public float degreesPerSecond = 4.0f;
-    public float amplitude = 0.1f;
-    public float frequency = 0.5f;
+    public float frequency = 0.1f;
+    public float duration;
+    public Material mArm;
+    public Color initialC;
+    public Color targetC;
+
+    public UnityEvent afterArm;
 
     // Position Storage Variables
-    Vector3 posOffset = new Vector3();
-    Vector3 tempPos = new Vector3();
+    private Vector3 posOffset = new Vector3();
+    private Vector3 tempPos = new Vector3();
+    private float amplitude;
+    private bool objectAppear;
 
     // Use this for initialization
-    void Start()
+    void Awake()
     {
-        // Store the starting position & rotation of the object
-        posOffset = transform.position;
+        mArm.color = initialC;
+        amplitude = 0.02f;
+        objectAppear = false;
+        this.gameObject.SetActive(false);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Spin object around Y-Axis
-        transform.Rotate(new Vector3(0f, Time.deltaTime * degreesPerSecond, 0f), Space.World);
+        if (objectAppear == true)
+        {
+            // Store the starting position & rotation of the object
+            posOffset = transform.position;
 
-        // Float up/down with a Sin()
-        tempPos = posOffset;
-        tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+            // Spin object around Y-Axis
+            transform.Rotate(new Vector3(0f, Time.deltaTime * degreesPerSecond, 0f), Space.World);
 
-        transform.position = tempPos;
+            // Float up/down with a Sin()
+            tempPos = posOffset;
+            tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+
+            transform.position = tempPos;
+        }
+    }
+
+    public void ObjAppear()
+    {
+        objectAppear = true;
+        this.gameObject.SetActive(true);
+        StartCoroutine(armTimeline());
+    }
+
+    IEnumerator armTimeline()
+    {
+        StartCoroutine(LerpMColor(targetC));
+
+        yield return new WaitForSeconds(duration);
+
+        StartCoroutine(LerpMColor(initialC));
+
+        yield return new WaitForSeconds(duration);
+
+        objectAppear = false;
+        afterArm.Invoke();
+    }
+
+    /*IEnumerator LerpAmplitude(float endValue)
+    {
+        float time = 0;
+        while (time < duration)
+        {
+            amplitude = Mathf.Lerp(amplitude, endValue, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+            
+        }
+        amplitude = endValue;
+    }*/
+
+    IEnumerator LerpMColor(Color targetColor)
+    {
+        float time = 0;
+        Color startColor = mArm.color;
+        while (time < duration)
+        {
+            mArm.color = Color.Lerp(startColor, targetColor, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        mArm.color = targetColor;
     }
 }
